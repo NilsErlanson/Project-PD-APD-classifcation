914c333d3596288fe0f97113635522497cba6316
NilsErlanson <nils.erlanson@gmail.com>
2019-11-13 16:04:14 +0100

    visFuncs with visfuncs

--- visFuncs.py
+++ visFuncs.py
@@ -38,18 +38,54 @@ def scroll_slices(sample):
     fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
     plt.show()
 
-def show_scan(sample,sliceNr=10):
-    images = np.squeeze(sample[0])
-    label = sample[1]
+def get_name(label):
     
-    fig, axs = plt.subplots(2, 2, figsize=(10,10))
+    if(np.array_equal(label,[1, 0, 0, 0, 0, 0])):
+        name = 'Frisk'
+    elif(np.array_equal(label,[0, 1, 0, 0, 0, 0])):
+        name = 'Vaskulär Parkinomism'
+    elif(np.array_equal(label,[0, 0, 1, 0, 0, 0])):
+        name = 'PD'
+    elif(np.array_equal(label,[0, 0, 0, 1, 0, 0])):
+        name = 'Lewy Body Disease'
+    elif(np.array_equal(label,[0, 0, 0, 0, 1, 0])):
+        name = 'FTD'
+    else:
+        name = 'NPH'
+    return name
 
-    axs[0, 0].imshow(images[:,:,sliceNr]) #SUVr
-    axs[0, 0].set_title(['Slicenumber', sliceNr])
-    axs[0, 1].imshow(images[:,:,sliceNr+30]) #rCBF
-    axs[0, 1].set_title(['Slicenumber', sliceNr+30])
-    axs[1, 0].imshow(images[:,:,sliceNr+60]) #SUVr
-    axs[1, 0].set_title(['slicenumber ', sliceNr+60])
-    axs[1, 1].imshow(images[:,:,sliceNr+90]) #rCBF
-    axs[1, 1].set_title(['slicenumber ', sliceNr+90])
+
+def show_scan(sample,sliceNr=10,show = True):
+    images = sample[0]
+    label = sample[1]
+    name = get_name(label)
+    img_suvr = images[...,0]
+    img_rbf = images[...,1]
+    fig, axs = plt.subplots(2, 3, figsize=(10,10))
+
+
+    #SUVR first row
+    axs[0,0].imshow(img_suvr[:,:,sliceNr],cmap='bone') #SUVr
+    axs[0,0].set_title(['SUVR, Slicenumber: ', sliceNr])
+    axs[0,1].imshow(img_suvr[:,:,sliceNr+40],cmap = 'bone') #SUVr
+    axs[0,1].set_title(['suvr, slicenumber: ', sliceNr+40])
+    axs[0,2].imshow(img_suvr[60,:,:],cmap = 'bone')
+    axs[0,2].set_title('rotated')
+    #RBF secod row
+    axs[1, 0].imshow(img_rbf[:,:,sliceNr],cmap = 'bone') #rCBF
+    axs[1, 0].set_title(['rcbf, slicenumber: ', sliceNr])
+    axs[1, 1].imshow(img_rbf[:,:,sliceNr+39],cmap = 'bone') #rCBF
+    axs[1, 1].set_title(['rcbf, slicenumber ', sliceNr+40])
+    axs[1, 2].imshow(img_rbf[60,:,:],cmap = 'bone') #rCBF
+    axs[1, 2].set_title(['rcbf, rotated '])
+    
+    plt.suptitle(name)
+    if(show):
+        plt.show()
+
+def comp_samples(sample1,sample2):
+
+    show_scan(sample1,show = False)
+
+    show_scan(sample2,show = False)
     plt.show()
