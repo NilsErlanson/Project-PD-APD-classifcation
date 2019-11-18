d5fbd46d096d2be4ee0667c0cc5b7bda61228b57
Andreas Lycksam <lycksam95@gmail.com>
2019-11-18 09:24:58 +0100

    the split function works when a batck_size=1 is used

--- training.py
+++ training.py
@@ -21,25 +21,42 @@ import torch
 from torch.utils.data import DataLoader, random_split
 
 
+# TEST PRETRAINED MODELS
+import torchvision
+
+
 def training_session(model, optimizer, cost_function, train_data, test_data):
+    print("Training...")
     # track the training and test loss
     training_loss = []
     test_loss = []
+
+    # Convert the data
+    #if torch.cuda.is_available() and config.USE_CUDA:
+    #    dtype = torch.cuda.FloatTensor()
+    #else:
+    #    dtype = torch.FloatTensor()
 
     # optimize parameters for 3 epochs
     for i in range(config.epochs):
 
         # for each minibatch
         for x, y in train_data:
+
+            # Convert the data into the right format
             x = x.permute(0,4,1,2,3)
+            
+            #x = x.dtype
+            #y = y.dtype
+
+            # MED EGEN SPLITTNING
+            x = x.float()
+            y = y.float()
 
             # evaluate the cost function on the training data set
-
-            #loss = cost_function(model(x), torch.max(y, 1)[1]) #need model output and target
-            #loss = criterion(outputs, torch.max(labels, 1)[1])
             target = torch.max(y,1)[1] #needed for crossentropy
-            #print("target: ",y)
-
+                    
+        
             loss = cost_function(model.forward(x), target)
             #print("Loss: " ,loss)
             # update the statistics
@@ -59,21 +76,38 @@ def training_session(model, optimizer, cost_function, train_data, test_data):
         with torch.no_grad():
 
             #evaluate the cost function on the test data set
-           accumulated_loss = 0
-           for x, y in test_data:
-               x = x.permute(0,4,1,2,3)
-               target = torch.max(y,1)[1]
-               loss = cost_function(model.forward(x), target)
-               accumulated_loss += loss.item()
+            accumulated_loss = 0
+            for x, y in test_data:
+                x = x.permute(0,4,1,2,3)
+                x = x.float()
+                y = y.double()
+                target = torch.max(y,1)[1]
+                loss = cost_function(model.forward(x), target)
+                accumulated_loss += loss.item()
                 
             #update the statistics
-           test_loss[-1] = accumulated_loss / len(test_data)
+            test_loss[-1] = accumulated_loss / len(test_data)
                 
         print(f"Epoch {i + 1:2d}: training loss {training_loss[-1]: 9.3f},"f"test loss {test_loss[-1]: 9.3f}")
+    
+    print("Done!")
+    
+    plot_training_test_loss(training_loss, test_loss)
 
     return model
 
-        
+def plot_training_test_loss(training_loss, test_loss):
+    import matplotlib.pyplot as plt
+    # plot loss
+    plt.figure()
+    iterations = np.arange(1, len(training_loss) + 1)
+    plt.scatter(iterations, training_loss, label='training loss')
+    plt.scatter(iterations, test_loss, label='test loss')
+    plt.legend()
+    plt.xlabel('iteration')
+    plt.show()   
+
+
 # ************************** Ligger i main ***************************
 if __name__ == "__main__":
     # Load the datasets
@@ -82,36 +116,46 @@ if __name__ == "__main__":
     # Split into training and test dataset
     train_dataset, test_dataset = load_dataset.split_dataset_one_random_sample_from_each_class(original_dataset, augmented_dataset)
 
-    print("len(original_dataset): ", len(original_dataset))
-    print("len(augmented_dataset): ", len(augmented_dataset))
-    print("len(train_dataset): ", len(train_dataset))
-    print("len(test_dataset): ", len(test_dataset))
+    print("len(original_dataset): ", len(original_dataset), " type ", original_dataset)
+    print("len(augmented_dataset): ", len(augmented_dataset), " type ", augmented_dataset)
+    #print("len(train_dataset): ", len(train_dataset), " type ", train_dataset)
+    #print("len(test_dataset): ", len(test_dataset), " type ", test_dataset)
 
-    print(type(train_dataset.__getitem__(0)[0]))
 
+    print("\n\n")
     # THIS DATASPLIT WORKS
-    #dataset = original_dataset
+    #dataset = augmented_dataset
     #train_size = int(0.8 * len(dataset))
     #test_size = len(dataset) - train_size
     #train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
     #print(len(train_dataset), len(test_dataset))
-    
+    #print("FROM ORIGINAL: len(train_dataset): ", len(train_dataset), " type ", train_dataset)
+    #print("FROM ORIGINAL: len(test_dataset): ", len(test_dataset), " type ", test_dataset)
+    #print(type(train_dataset.__getitem__(0)[0][1,1,1,0]))
+    #print("\n\n")
+
     # define the data loaders
-    train_data = torch.utils.data.DataLoader(train_dataset, batch_size = config.batchSize, shuffle=True)
-    test_data = torch.utils.data.DataLoader(test_dataset, batch_size = config.batchSize)
-      
+    #train_data = torch.utils.data.DataLoader(train_dataset, batch_size = config.batchSize, shuffle=True)
+    #test_data = torch.utils.data.DataLoader(test_dataset, batch_size = config.batchSize)
+
+    train_data = torch.utils.data.DataLoader(train_dataset, batch_size = 1, shuffle = True)
+    test_data = torch.utils.data.DataLoader(test_dataset, batch_size = 1)
+
+
     # define the model
-    model = model.SimpleCNN()
-    #output = model.forward(testtensor)
-    #print(output)
+    model = model.SimpleCNN() 
+    #model = model.UNet3D(2, config.nrOfDifferentDiseases)  
+    #model = torchvision.models.googlenet(pretrained=False, progress=True)
+ 
 
     # USES FLOAT
     from torchsummary import summary
     summary(model, input_size = (2, 128, 128, 80))
 
+
     # TRAINING USES DOUBLE
-    model = model.double()
- 
+    #model = model.double()
+
     # define the cost function
     #cost_function = nn.MSELoss()
     cost_function = torch.nn.CrossEntropyLoss()
@@ -120,8 +164,32 @@ if __name__ == "__main__":
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
     # run training
-    model = training_session(model, optimizer, cost_function, train_data, test_data)
+    trained_model = training_session(model, optimizer, cost_function, train_data, test_data)
 
+    #torch.save(trained_model)
+
+    trained_model.eval()
+    
+    """
+    # TEST
+    test_input = train_dataset.__getitem__(0)[0]
+    test_target = train_dataset.__getitem__(0)[1]
+    #Convert input
+    test_input = torch.tensor(test_input).float()
+    test_input = test_input.unsqueeze(0)
+    test_input = test_input.permute(0,4,1,2,3) 
+    print(type(test_input), test_input)
+    #Convert target
+    print(type(test_target), test_target)
+    test_target = torch.tensor([2])
+
+    model_test = model(test_input)
+    cost_test = cost_function(model(test_input), test_target)
+    # TEST
+    """
+
+
+    """
     test = dataset[9]
     scan = test[0]
     scan = torch.from_numpy(scan).unsqueeze_(0)
@@ -139,3 +207,5 @@ if __name__ == "__main__":
     print(prediction)
     print(ind)
     
+    """
+
