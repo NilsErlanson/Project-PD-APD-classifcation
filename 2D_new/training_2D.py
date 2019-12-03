d9211034398ed6e6db261ab2595640ed8ac0bfb1
Andreas Lycksam <lycksam95@gmail.com>
2019-12-03 10:38:06 +0100

    för att visa torsten

--- 2D_new/training_2D.py
+++ 2D_new/training_2D.py
@@ -237,9 +237,9 @@ if __name__ == "__main__":
 
     # Load the datasets from the pickle file   
     original_dataset = load_dataset_2D.load_original_dataset()
-
+    """
     # Split the original dataset into two subsets, training/testing
-    train_size = int(0.8* len(original_dataset))
+    train_size = int(0.9* len(original_dataset))
     test_size = len(original_dataset) - train_size
     train_dataset, test_dataset = random_split(original_dataset, [train_size, test_size])
 
@@ -271,7 +271,7 @@ if __name__ == "__main__":
                                                         useMultipleSlices = config_2D.useMultipleSlices, 
                                                         mirrorImage = config_2D.applyMirrorImage, 
                                                         gammaTransform = config_2D.gamma, 
-                                                        transform = config_2D.transform_train)
+                                                        transform = config_2D.transform_test)
     
     # For unbalanced dataset we create a weighted sampler                       
     class_sample_count = find_class_sample_count(train_dataset)
@@ -303,9 +303,9 @@ if __name__ == "__main__":
     validate(device, test_data, trained_model)
     
     # Validate the robustness of the model
-    accuracy, train_losses, test_losses = kfold(device, original_dataset)
-    print(accuracy)
-    
+    #accuracy, train_losses, test_losses = kfold(device, original_dataset)
+    #print(accuracy)
+    """
 
     """
     # Multiple plots to examine the data
@@ -330,8 +330,6 @@ if __name__ == "__main__":
             axs2[i, j].imshow(sample2[0][1,:,:], cmap = cmap, vmin = 0, vmax = 1)
             axs2[i, j].set_title([name])
             axs2[i,j].axis('off')
-
-            print("TRAINING: "," max suvr: ", sample2[0][0,:,:].max(), " max rcbf: ", sample2[0][1,:,:].max(), " label : ", sample2[1])
     
     for i in range(4):
             sample2 = test_dataset.__getitem__(i)
@@ -351,45 +349,200 @@ if __name__ == "__main__":
             axs4[i,0].axis('off')
             axs4[i,1].axis('off')
 
-            print("TEST: "," max suvr: ", sample2[0][0,:,:].max(), " max rcbf: ", sample2[0][1,:,:].max(), " label : ", sample2[1])
     plt.show()
     """
-    """
+    # Normalize the training/test datasets
+    original_dataset2 = transformations_2D.ApplyNormalization(original_dataset, None, None, True)
+    
+    # Apply transforms to the training/test datasets
+    original_dataset2 = transformations_2D.ApplyTransform(original_dataset2, 
+                                                        sliceNr = config_2D.sliceSample, 
+                                                        applyMean = config_2D.addMeanImage, 
+                                                        applyDiffnormal = config_2D.applydiffnormal,
+                                                        meantrainbrain_rcbf = None, 
+                                                        meantrainbrain_suvr = None, 
+                                                        useMultipleSlices = config_2D.useMultipleSlices, 
+                                                        mirrorImage = config_2D.applyMirrorImage, 
+                                                        gammaTransform = config_2D.gamma, 
+                                                        transform = config_2D.transform_train)
+    
+    from copy import copy
+    import cv2
+    import matplotlib.colors as colors
+
+    palette = copy(plt.cm.jet)
+    palette.set_under('darkblue', 0.8)
+    palette.set_under('black', 0.7)
+    palette.set_under('black', 1.0)
+
     # Multiple plots to examine the data
     fig, axs = plt.subplots(4, 5, figsize = (15,6))
     fig, axs2 = plt.subplots(4, 5, figsize = (15,6))
+    fig, axs3 = plt.subplots(4, 5, figsize = (15,6))
+    fig, axs4 = plt.subplots(4, 5, figsize = (15,6))
 
-    for x in range( len(original_dataset) ):
-        sample = original_dataset.__getitem__(x)
-        #print("label", x, ": ", sample[1])
-        
-
-    from medpy.filter import otsu
-    cmap = 'CMRmap'
-    sliceNumber = 64
+    cmap = 'jet'
     for i in range(4):
         for j in range(5):
-            sample2 = original_dataset.__getitem__(j+i*5)
-
-            image_otsu_suvr = sample2[0][:,:,sliceNumber,0]
-            threshold = otsu(image_otsu_suvr)
-            print("threshold: ", threshold)
-            image_otsu_suvr_output = image_otsu_suvr > threshold
-        
-            image_otsu_rcbf = sample2[0][:,:,sliceNumber,1]
-            threshold = otsu(image_otsu_rcbf)
-            image_otsu_rcbf_output = image_otsu_rcbf > threshold
- 
+            sample2 = original_dataset2.__getitem__(j+i*5)
 
             name = visFuncs_2D.get_name(sample2[1])
-            axs[i, j].imshow(image_otsu_suvr_output, cmap = cmap, vmin = 0, vmax = 1)
+            #axs[i, j].imshow(sample2[0][0,:,:], cmap = cmap, vmin = 0.005, vmax = 1)
+            axs[i, j].imshow(sample2[0][0,:,:], cmap = palette, vmin = 0.0005, vmax = 1)  
+
             axs[i, j].set_title([name])
             axs[i, j].axis('off')
 
-            axs2[i, j].imshow(image_otsu_rcbf_output, cmap = cmap, vmin = 0, vmax = 1)
+            #test2 = axs2[i, j].imshow(sample2[0][1,:,:], cmap = cmap, vmin = 0.005, vmax = 1)
+            axs2[i, j].imshow(sample2[0][1,:,:], cmap = palette, vmin = 0.0005, vmax = 1)  
             axs2[i, j].set_title([name])
             axs2[i, j].axis('off')
 
+            #images = sample2[0].numpy()
+            #images[0,:,:] = cv2.flip(images[0,:,:], 0)
+            #images[1,:,:] = cv2.flip(images[1,:,:], 0)
+
+            axs3[i, j].imshow(sample2[0][2,:,:], cmap = palette, vmin = 0.0005, vmax = 1)  
+            axs3[i, j].set_title([name])
+            axs3[i, j].axis('off')
+
+            #test2 = axs2[i, j].imshow(sample2[0][1,:,:], cmap = cmap, vmin = 0.005, vmax = 1)
+            axs4[i, j].imshow(sample2[0][3,:,:], cmap = palette, vmin = 0.0005, vmax = 1)  
+            axs4[i, j].set_title([name])
+            axs4[i, j].axis('off')    
+
     plt.show()
+
+
     """
- 
+    # plot to save for the report
+    from copy import copy
+    import matplotlib.colors as colors
+
+    # Get the sample to be saved
+    sample2 = original_dataset2.__getitem__(7)
+    zero_healthy = sample2[0][0,:,:]
+    one_healthy = sample2[0][1,:,:]
+
+    
+    # set the colormap
+    cmap = 'jet'
+    palette = copy(plt.cm.jet)
+    palette.set_under('darkblue', 0.8)
+    palette.set_under('black', 0.7)
+    palette.set_under('black', 1.0)
+
+
+    fig = plt.figure(num=None, figsize=(8, 12), dpi = 80, facecolor='w', edgecolor='k')
+    ax = fig.add_subplot(111)
+    ax.axis('off')
+    
+    #name = visFuncs_2D.get_name(sample2[1])
+    #ax.set_title(name)
+
+
+    #cbaxes = fig.add_axes([0.1, 0.1, 0.03, 0.8])  # This is the position for the colorbar
+    
+    image_report = ax.imshow(one_healthy, cmap = palette, vmin = 0, vmax = 1)  
+    cb = plt.colorbar(image_report, cmap = palette, fraction = 0.046, pad = 0.04, shrink = 0.9)
+
+    #cb = plt.colorbar(image_report, cmap = palette, cax = cbaxes, fraction = 0.046, pad = 0.04)
+    cb.ax.set_yticklabels(['0', '0.2', '0.4', '0.6', '0.8', '1'])  # vertically oriented colorbar
+    cb.ax.tick_params(labelsize=14)
+
+
+    ax.remove()
+    #plt.savefig("../figures/colorbar.png", bbox_inches = 'tight', pad_inches = 0)
+
+    plt.show()
+
+    #fig = plt.figure(num=None, figsize=(8, 6), dpi = 80, facecolor='w', edgecolor='k')
+    #ax = fig.add_subplot(111)
+    #ax.axis('off')
+    #image_report = ax.imshow(zero_healthy, cmap = palette, vmin = 0.007, vmax = 1)  
+    #plt.savefig("../figures/PD_radioactivity.eps", bbox_inches = 'tight', pad_inches = 0)
+
+    #plt.show()
+    """
+
+
+
+    # STACKED IMAGES
+    """
+    from copy import copy
+    import matplotlib.pyplot as plt
+    import matplotlib.colors as colors
+    from scipy.ndimage.interpolation import affine_transform
+
+
+    fig = plt.figure(num=None, figsize=(16, 6), dpi = 80, facecolor='w', edgecolor='k')
+    ax = fig.add_subplot(111)
+    ax.axis('off')
+
+    # set the colormap
+    cmap = 'jet'
+    palette = copy(plt.cm.jet)
+    palette.set_under('darkblue', 0.8)
+    palette.set_under('black', 0.7)
+    palette.set_under('black', 1.0)
+
+    # Specify the number of images
+    nimages = 4
+    img_height, img_width = 128, 128
+    bg_val = -0.1 # Some flag value indicating the background.
+
+    # Random test images.
+    sample = original_dataset2.__getitem__(16)
+    # Put the images into a list
+    images = [sample[0][3,:,:], sample[0][2,:,:], sample[0][1,:,:], sample[0][0,:,:]]
+
+    stacked_height = 2*img_height
+    stacked_width  = int(img_width + (nimages-1) * img_width * 1.25)
+    stacked = np.full((stacked_height, stacked_width), bg_val)
+
+    # Affine transform matrix.
+    T = np.array([[1, -0.7],
+                [0, 0.7]])
+
+    for i in range(nimages):
+        # The first image will be right most and on the "bottom" of the stack.
+        #o = (nimages-i-1) * img_width/2
+        o = (nimages-i-1) * img_width / 1.3
+
+        out = affine_transform(images[i], T, offset = [o,-o], output_shape = stacked.shape, cval = bg_val)
+        stacked[out != bg_val] = out[out != bg_val]
+
+    plt.imshow(stacked, cmap = palette, vmin = 0.0035, vmax = 1)
+    plt.savefig("../figures/multiple_slices_healthy.png", bbox_inches = 'tight', pad_inches = 0)
+    plt.show()
+    """ 
+   
+   
+    """
+    # TRAIN THE NETWORK USING ALL SAMPLES PRETRAINED
+
+    # Extracting features
+    layer = model._modules.get('layer1')
+    model.eval()
+
+
+    def getFeatures(sample):
+        scans, label = sample 
+        scans = scans.unsqueeze(0).to(device).float() # take it to the correct form for the model
+        my_embedding = torch.zeros((1,256,56,56)) # this is depending on which layer the model looks at CHANGE THIS IF WANT TO USE ANOTHER LAYER
+        # 4. Define a function that will copy the output of a layer
+
+        def copy_data(m, i, o):
+            my_embedding.copy_(o.data)
+            
+        # 5. Attach that function to our selected layer
+        h = layer.register_forward_hook(copy_data)
+        # 6. Run the model on our transformed image
+        model.forward(scans)
+        # 7. Detach our copy function from the layer
+        h.remove()
+        # 8. Return the feature vector
+        return my_embedding.squeeze(0) #remove batch dimension for visualization
+
+    #plot my_embedding_squeeze(0)   
+    """
